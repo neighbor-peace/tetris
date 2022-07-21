334e516fde50a48f19a78e6d3e03df07a5e76b92
Michael Costello <mcostello91@gmail.com>
2022-07-21 12:59:00 -0700

    Render new tetromino after merge

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /*
 TODO
-new shape after collision
 game over when reach the top
 clear row when filled
   -delete row
@@ -20,18 +19,55 @@ context.scale(20, 20);
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
-
 const player = {
   pos: {x: 5, y: 0},
-  matrix: matrix,
+  tetromino: assignTetromino(),
 }
 
 const arena = createMatrix(12, 20);
+
+function assignTetromino() {
+  //create array of tetrominos
+  const tetrominoArray = [
+    [
+      [0,0,0],
+      [1,1,1],
+      [0,1,0]
+    ],
+    [
+      [1,1],
+      [1,1]
+    ],
+    [
+      [0,0,1,0,0],
+      [0,0,1,0,0],
+      [0,0,1,0,0],
+      [0,0,1,0,0]
+    ],
+    [
+      [0,1,0,0],
+      [0,1,1,0],
+      [0,0,1,0]
+    ],
+    [
+      [0,0,1,0],
+      [0,1,1,0],
+      [0,1,0,0]
+    ],
+    [
+      [0,1,0],
+      [0,1,0],
+      [1,1,0],
+    ],
+    [
+      [0,1,0],
+      [0,1,0],
+      [0,1,1],
+    ]
+  ]
+  //return tetromino from random index
+  return tetrominoArray[Math.floor(Math.random() * tetrominoArray.length)];
+}
 
 function createMatrix(x, y) {
   const matrix = [];
@@ -42,7 +78,7 @@ function createMatrix(x, y) {
 }
 
 function merge(arena, player) {
-  player.matrix.forEach((row, y) => {
+  player.tetromino.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value) {
         arena[y + player.pos.y][x + player.pos.x] = value;
@@ -52,7 +88,7 @@ function merge(arena, player) {
 }
 
 function detectCollision(arena, player) {
-  const [m, o] = [player.matrix, player.pos];
+  const [m, o] = [player.tetromino, player.pos];
   for (let y = 0; y < m.length; y++) {
     for (let x = 0; x < m[y].length; x++) {
       if (m[y][x] !== 0 &&
@@ -64,7 +100,6 @@ function detectCollision(arena, player) {
   }
   return false;
 }
-
 
 function drawMatrix(matrix, offset = {x: 0, y: 0}) {
   matrix.forEach((row, y) => {
@@ -81,7 +116,7 @@ function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
   drawMatrix(arena);
-  drawMatrix(player.matrix, player.pos)
+  drawMatrix(player.tetromino, player.pos)
 }
 
 let dropCounter = 0;
@@ -106,24 +141,24 @@ function dropPlayer() {
     merge(arena, player);
     player.pos.y = 0;
     player.pos.x = 5;
+    player.tetromino = assignTetromino();
   }
   dropCounter = 0;
 }
 
-function rotate(count) {
+function rotateTetromino(count) {
   while (count--) {
-  const newMatrix = [];
-  for (let y = 0; y < player.matrix.length; y++) {
-    newMatrix.push([]);
-  }
-
-  for (let y = player.matrix.length - 1; y >= 0; y--) {
-    for (let x = 0; x < player.matrix[y].length; x++) {
-      newMatrix[x].push(player.matrix[y][x]);
+    const newTetromino = [];
+    for (let y = 0; y < player.tetromino[0].length; y++) {
+      newTetromino.push([]);
     }
+    for (let y = player.tetromino.length - 1; y >= 0; y--) {
+      for (let x = 0; x < player.tetromino[y].length; x++) {
+        newTetromino[x].push(player.tetromino[y][x]);
+      }
+    }
+    player.tetromino = newTetromino;
   }
-  player.matrix = newMatrix;
-}
 }
 
 document.addEventListener('keydown', event => {
@@ -145,9 +180,9 @@ document.addEventListener('keydown', event => {
       break;
     case "PageUp": //counter clockwise
     case "PageDown": //clockwise
-      event.code === "PageUp" ? rotate(3) : rotate(1);
+      event.code === "PageUp" ? rotateTetromino(3) : rotateTetromino(1);
       if (detectCollision(arena, player)) {
-        event.code === "PageUp" ? rotate(1) : rotate(3);
+        event.code === "PageUp" ? rotateTetromino(1) : rotateTetromino(3);
       }
       break;
   }
